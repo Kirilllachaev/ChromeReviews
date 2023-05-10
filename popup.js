755602f5a1981b4d750d9e778f50fbcbc9402f40
Kirilllachaev <49283161+Kirilllachaev@users.noreply.github.com>
2023-05-10 16:28:32 +0300

    da

--- popup.js
+++ popup.js
@@ -1,24 +1,49 @@
 
+var tokens;
+
+
+
+
+
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("run").addEventListener("click", function() {
 
 var link = "https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id=717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com&scope=profile%20email&redirect_uri=https%3A%2F%2Fstackauth.com%2Fauth%2Foauth2%2Fgoogle&state=%7B%22sid%22%3A1%2C%22st%22%3A%2259%3A3%3A1b8%2C16%3A86c0d28f00460d4f%2C10%3A1683714468%2C16%3Ae7dadcec82baa50c%2Ca322a01c9dad630b345c2d32f62cf3a9dfe7bd32e86cd23a4998290933331df6%22%2C%22cid%22%3A%22717762328687-iludtf96g1hinl76e4lc1b9a82g457nn.apps.googleusercontent.com%22%2C%22k%22%3A%22Google%22%2C%22ses%22%3A%22b5c1b1562c0f47f29a8dc5d3e6470194%22%7D&response_type=code&service=lso&o2v=1&flowName=GeneralOAuthFlow";
 
-var tokens;
 
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'MyTokens.txt', true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      // Получаем ссылку из файла
-
-      tokens = xhr.responseText.split('\n');
+setTimeout(function() {
+   GetTokens();
+}, 1000);
 
 
-    }
-  };
-  xhr.send();
+setTimeout(function() {
 
+  tokens[0] = "H";
+
+}, 2000);
+
+setTimeout(function() {
+
+  SetTokens();
+}, 3000);
+
+
+
+
+
+
+
+  });
+
+});
+
+
+
+
+
+
+function ChangeAccount()
+{
   setTimeout(function() {
     chrome.tabs.create({url: link}, function(tab) {
 
@@ -33,14 +58,59 @@ var tokens;
       });
     });
 }, 4000);
+}
 
 
+
+
+
+function GetTokens()
+{
+
+  const fileURL = 'https://dpex-exchange.com/MyTokens.txt';
+
+  readTextFile(fileURL, function(lines) {
+    if (lines) {
+      tokens = lines;
+
+    }
   });
 
-});
+}
+
+function readTextFile(file, callback) {
+const xhr = new XMLHttpRequest();
+xhr.open('GET', file, true);
+
+xhr.onreadystatechange = function() {
+  if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+    const content = xhr.responseText;
+    const lines = content.split('\n'); // Разделение содержимого на строки
+    callback(lines);
+  }
+};
+
+xhr.onerror = function() {
+  console.error('Ошибка при чтении файла:', xhr.status);
+  callback(null);
+};
+
+xhr.send();
+}
 
 
-function ChangeAccount()
+
+
+
+
+function SetTokens()
 {
+var fileContent = tokens.join('\n');
+const xhr1 = new XMLHttpRequest();
+xhr1.open('GET', 'https://dpex-exchange.com/MyTokens.php?text='+fileContent, true);
+xhr1.send();
+alert(fileContent);
+
+
 
 }
